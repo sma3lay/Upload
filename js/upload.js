a428ea8ef276eecd67125157ea4236a591886f56
sma3lay <93422454+sma3lay@users.noreply.github.com>
2022-10-29 10:25:51 +0630

    Add files via upload

---
+++ js/upload.js
@@ -0,0 +1,30 @@
+function reportInfo(vars, showType = false) {
+    if (showType === true) console.log(typeof vars);
+    console.log(vars);
+}
+
+function addImg(ele, content) {
+    var myDIV = document.querySelector(ele);
+    var newContent = document.createElement('div');
+    newContent.innerHTML = content;
+
+    while (newContent.firstChild) {
+        myDIV.appendChild(newContent.firstChild);
+    }
+}
+
+var feedback = function(res) {
+    reportInfo(res, true);
+    if (res.success === true) {
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        document.querySelector('.status').classList.add('bg-success');
+        var content =
+             '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+        addImg('.status', content);
+    }
+};
+
+new Imgur({
+    clientid: 'b9276692d1f0913', //You can change this ClientID
+    callback: feedback
+});
